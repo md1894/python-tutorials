d4ff3b4157a404d79b69c5dbace8a202e5f711d0
Mehul <you@example.com>
2019-10-21 17:50:24 +0530

    added function/function1.py

---
+++ function/function1.py
@@ -0,0 +1,4 @@
+
+# we have to use two keywords 1. def 2. return while defining function in python
+# return keyword is optional
+# when function is not returning any value , None object gets returned
