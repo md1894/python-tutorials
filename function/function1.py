8bee08383667a1feec3f9e59d8a12bf30876f450
Mehul <you@example.com>
2019-10-21 18:00:48 +0530

    initial changes

--- function/function1.py
+++ function/function1.py
@@ -1,4 +1,21 @@
 
-# we have to use two keywords 1. def 2. return while defining function in python
+# we have to use two keywords (1.def , 2.return) while defining function in python
 # return keyword is optional
 # when function is not returning any value , None object gets returned
+
+def noArgNoReturn():
+    print('-------------------------------------------------------------------------------------------')
+    print('inside noArgNoReturn()')
+    print('this is a function in which you dont have to pass any argument , also it wont return any value')
+
+def noArgButReturnValue():
+    print('--------------------------------------------------------------------------------------------')
+    print('inside noArgButReturnValue()')
+    # you can return any value , for demo purpose i am returning str value
+    return 'this is a string that gets returned from noArgButReturnValue()'
+
+x = noArgNoReturn()
+print('value that gets returned from noArgNoReturn() is',x)
+
+x = noArgButReturnValue()
+print('value that gets returned from noArgButReturnValue() is--->>',x)
